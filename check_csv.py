c8c1b63c2ce5295899deff93b9c8148a463b2878
Claire Pan <115050875+clairepan1@users.noreply.github.com>
2023-04-23 00:14:23 -0400

    Update check_csv.py

--- check_csv.py
+++ check_csv.py
@@ -3,7 +3,7 @@ import datetime as datetime
 import subprocess
 
 # Define the directory where the CSV files are stored
-directory = '/data'
+directory = '/data/'
 
 # Define the start and end dates of the sequence
 start_date = '20220801'
@@ -20,12 +20,12 @@ while current_date <= end_date:
     if not os.path.exists(os.path.join(directory, top_left_file)):
         print(f"Missing file: {top_left_file}")
         # Generate the CSV file using generate_csv.py
-        subprocess.run(['python', 'generate_csv.py'])
+        subprocess.run(['python', 'generate_data.py'])
     
     if not os.path.exists(os.path.join(directory, top_right_file)):
         print(f"Missing file: {top_right_file}")
         # Generate the CSV file using generate_csv.py
-        subprocess.run(['python', 'generate_csv.py'])
+        subprocess.run(['python', 'generate_data.py'])
 
     # Increment the current date by one week
     current_date = (datetime.datetime.strptime(current_date, '%Y%m%d') + datetime.timedelta(weeks=1)).strftime('%Y%m%d')
